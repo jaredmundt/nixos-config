fd3ea84f53e6d3cf837c35f206863ba84b5069d1
Jared Mundt <jaredmundt@gmail.com>
2024-09-04 14:29:11 -0600

    add git, vim, wget

--- configuration.nix
+++ configuration.nix
@@ -93,8 +93,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim
+    wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
